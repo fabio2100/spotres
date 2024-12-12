e47f287cb61a5b8d31da0478acad25b5f9ba2cb0
Fabio Squizzato <fsquizzato@visualmedica.com>
2024-12-12 16:08:20 -0300

    first commit: Read user one list

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,86 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 import styles from "./page.module.css";
+import axios from "axios";
 
-export default function Home() {
+export default function HomePage() {
+  const router = useRouter();
+  const [hasCode, setHasCode] = useState(false);
+  const [token, setToken] = useState(null);
+  const [tracks, setTracks] = useState([]);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get("code");
+    if (code) {
+      setHasCode(true);
+      fetchToken(code);
+    }
+  }, []);
+
+  const fetchToken = async (code) => {
+    try {
+      const response = await axios.post(
+        "https://accounts.spotify.com/api/token",
+        null,
+        {
+          params: {
+            grant_type: "authorization_code",
+            code: code,
+            redirect_uri: "http://localhost:3000",
+            client_id: "72a784c03ba44304b249998a5ac3e899",
+            client_secret: "92b7aeeb47d0474ba73d5a186451358b",
+          },
+        }
+      );
+      setToken(response.data.access_token);
+      fetchTopTracks(response.data.access_token);
+    } catch (error) {
+      console.log("Error fetching token", error);
+    }
+  };
+
+  const fetchTopTracks = async (accessToken) => {
+    try {
+      const response = await axios.get(
+        "https://api.spotify.com/v1/me/top/tracks",
+        { headers: { Authorization: `Bearer ${accessToken}` } }
+      );
+      setTracks(response.data.items);
+    } catch (error) {
+      console.error("Error fetching the top tracks", error);
+    }
+  };
+
+  const handleLogin = () => {
+    window.location.href =
+      "https://accounts.spotify.com/authorize?client_id=72a784c03ba44304b249998a5ac3e899&response_type=code&redirect_uri=http://localhost:3000&scope=user-top-read";
+  };
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+      {" "}
+      <h1>Bienvenido a mi aplicación Next.js</h1>{" "}
+      {hasCode ? (
+        tracks.length > 0 ? (
+          <ul>
+            {" "}
+            {tracks.map((track) => (
+              <li key={track.id}>
+                {" "}
+                {track.name} by{" "}
+                {track.artists.map((artist) => artist.name).join(", ")}{" "}
+              </li>
+            ))}{" "}
+          </ul>
+        ) : (
+          <p>Validando código y obteniendo canciones...</p>
+        )
+      ) : (
+        <button onClick={handleLogin}>Login</button>
+      )}{" "}
     </div>
   );
 }
