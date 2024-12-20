46b4a73d6db715e9e5d7ceb77676ac7b8cddfedf
Fabio Squizzato <fsquizzato@visualmedica.com>
2024-12-20 16:12:06 -0300

    feature: update previous data

--- src/app/api/updateDB/route.js
+++ src/app/api/updateDB/route.js
@@ -8,18 +8,28 @@ export async function POST(req) {
   try {
     await client.query("BEGIN");
     const updateQuery = `
-      INSERT INTO user_data_spotify (user_name, data, created_at)
-      VALUES ($1, $2, CURRENT_TIMESTAMP)
-      ON CONFLICT (user_name) 
-      DO UPDATE SET data = EXCLUDED.data, created_at = EXCLUDED.created_at;
+        INSERT INTO user_data_spotify (user_name, data, created_at)
+        VALUES ($1, $2, CURRENT_TIMESTAMP)
+        ON CONFLICT (user_name) 
+        DO UPDATE SET 
+        previous_data = user_data_spotify.data,
+        data = EXCLUDED.data,
+        created_at = EXCLUDED.created_at;
+
     `;
     await client.query(updateQuery, [userEmail, userData]);
     await client.query("COMMIT");
-    return NextResponse.json({ message: "Datos actualizados exitosamente" }, { status: 200 });
+    return NextResponse.json(
+      { message: "Datos actualizados exitosamente" },
+      { status: 200 }
+    );
   } catch (error) {
     await client.query("ROLLBACK");
     console.error("Error actualizando la base de datos:", error);
-    return NextResponse.json({ error: "Error actualizando la base de datos" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Error actualizando la base de datos" },
+      { status: 500 }
+    );
   } finally {
     client.release();
   }
--- src/app/components/ListCustomItem.js
+++ src/app/components/ListCustomItem.js
@@ -1,11 +1,5 @@
 // components/CustomCard.js
 import * as React from "react";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 import { List, ListItem } from "@mui/material";
 import {
   IconButton,
@@ -13,7 +7,6 @@ import {
   Avatar,
   ListItemText,
 } from "@mui/material";
-import DeleteIcon from "@mui/icons-material/Delete";
 import FiberNewIcon from "@mui/icons-material/FiberNew";
 import { TbCircleArrowUpFilled, TbCircleArrowDownFilled } from "react-icons/tb";
 import { FaEquals } from "react-icons/fa";
